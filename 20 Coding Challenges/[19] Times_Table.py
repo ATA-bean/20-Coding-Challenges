25ee0f9ce02fba04b1215f33e3d463c003af8207
Abhinav Yadav <25abhinavyadav@gmail.com>
2025-02-27 18:27:23 +0000

    Delete 20 Coding Challenges/[19] Times_Table.py

--- 20 Coding Challenges/[19] Times_Table.py
+++
@@ -1,8 +0,0 @@
-def print_times_tables(times_table: int):
-    count = 1
-    while count < 13:
-        print(count,"x",times_table,"=",(times_table * count))
-        count += 1
-
-user = int(input("please enter a number:"))
-print_times_tables(user)
