457821cd6219c6525597c4dae2c40bc3403b7d14
Abhinav Yadav <25abhinavyadav@gmail.com>
2025-02-27 18:27:16 +0000

    Delete 20 Coding Challenges/[18] Squares.py

--- 20 Coding Challenges/[18] Squares.py
+++
@@ -1,8 +0,0 @@
-def print_squares(number_of_squares: int):
-    count = 1
-    while count < (number_of_squares + 1):
-        print(count,"squared is",(count**2))
-        count += 1
-
-user = int(input("please enter a number:"))
-print_squares(user)
